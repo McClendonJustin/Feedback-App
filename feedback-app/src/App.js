c875dd97ad031f1c8c2dced028e7402037cf7c84
Justin McClendon <mcclendon.justin.a@gmail.com>
2022-04-09 00:02:25 -0500

    Onto Forms

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -2,15 +2,24 @@ import React from 'react'
 import { useState } from 'react'
 import Header from './Components/Header'
 import FeedbackList from './Components/FeedbackList'
+import FeedbackStats from './Components/FeedbackStats'
 import FeedbackData from './data/FeedbackData'
 
 export default function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
+
     return (
         <>
             <Header />
             <div className='container'>
-                <FeedbackList feedback={feedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
         </>
     )
